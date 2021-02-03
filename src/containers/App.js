dfc15346d55395d42d165d6b2828157e17f20484
dfnino10 <df.nino10@uniandes.edu.co>
2021-02-03 14:30:30 -0500

    Deploying

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'
 
 class App extends Component {
@@ -24,10 +25,11 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter((robot) => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const {robots, searchField} = this.state;
+        const filteredRobots = robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
-        if(this.state.robots.length === 0){
+        if(!robots.length ){
             return <h1 className='tc'>Loading</h1>
         }else{
             return (
@@ -35,7 +37,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={ filteredRobots }/>
+                        <ErrorBoundry>
+                            <CardList robots={ filteredRobots }/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
