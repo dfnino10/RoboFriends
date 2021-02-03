345f937758bf4b912b56b4a4c059564004255680
dfnino10 <df.nino10@uniandes.edu.co>
2021-02-03 09:38:30 -0500

    Created card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({id, name, email}) => {
+    return(
+        <div className='bg-light-green dib br3 pa3 ma2 grow pw2 shadow-5 tc'>
+            <img src={`https://robohash.org/${id}?200x200`} alt='robot'/>
+            <div>
+                <h2 className=''>{name}</h2>
+                <p>{email}</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Card;
